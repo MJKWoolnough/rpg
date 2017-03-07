ee348ba9f75b257b6f30bbd3d56d75d0f947b14f
Michael Woolnough <michael.woolnough@gmail.com>
2017-03-07 16:59:30 +0000

    start of simple server

---
+++ server/server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"sync"
+
+	"github.com/MJKWoolnough/httpdir"
+	"golang.org/x/net/websocket"
+)
+
+var (
+	httpPort = flag.Int("http", -1, "http port")
+	tcpPort  = flag.Int("tcp", -1, "tcp port")
+)
+
+var dir http.FileSystem = httpdir.Default
+
+func ws(c *websocket.Conn) {
+	handleConn(c)
+}
+
+func handleConn(c net.Conn) {
+
+}
+
+func main() {
+	flag.Parse()
+	if *httpPort < 0 && *tcpPort < 0 {
+		flag.Usage()
+		return
+	}
+	s := new(State)
+	var wg sync.WaitGroup
+	if *httpPort >= 0 {
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", *httpPort))
+		if err != nil {
+			log.Fatal(err)
+		}
+		http.Handle("/ws", websocket.Handler(ws))
+		http.Handle("/", http.FileServer(dir))
+		wg.Add(1)
+		go func() {
+			log.Println(http.Serve(l, nil))
+			wg.Done()
+		}()
+		log.Println("HTTP server on %s", l.Addr())
+	}
+	if *tcpPort >= 0 {
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", *tcpPort))
+		if err != nil {
+			log.Fatal(err)
+		}
+		wg.Add(1)
+		go func() {
+			for {
+				c, err := l.Accept()
+				if err != nil {
+					if ne, ok := err.(net.Error); ok {
+						if ne.Temporary() {
+							continue
+						}
+					}
+					log.Println(err)
+					return
+				}
+				go handleConn(c)
+			}
+		}()
+		log.Println("TCP server on %s", l.Addr())
+	}
+	wg.Wait()
+}
