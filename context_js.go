a08e3d96c03c1dba88c9f6ee4bb475cb5c11e7a6
Michael Woolnough <michael.woolnough@gmail.com>
2017-03-14 15:01:03 +0000

    added simple SetColour method

--- context_js.go
+++ context_js.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/MJKWoolnough/gopherjs/xdom"
 	"github.com/MJKWoolnough/gopherjs/xjs"
 	"github.com/gopherjs/webgl"
@@ -26,4 +28,10 @@ func getContext(width, height int) (*Context, error) {
 
 func (c *Context) Close() error {
 	return nil
+}
+
+func (c *Context) SetColour(cl color.Color) {
+	r, g, b, a := cl.RGBA()
+	c.ctx.ClearColor(float32(r)/255, float32(g)/255, float32(b)/255, float32(a)/255)
+	c.ctx.Clear(0)
 }
