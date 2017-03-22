47253646c43d383c660bdc75a5ea66feb825a830
Michael Woolnough <michael.woolnough@gmail.com>
2017-03-22 09:51:04 +0000

    local (global) for webgl instance

--- main_js.go
+++ main_js.go
@@ -3,15 +3,19 @@
 package main
 
 import (
-	_ "github.com/MJKWoolnough/engine/windows/webgl"
+	"github.com/MJKWoolnough/engine/windows/webgl"
 	"github.com/MJKWoolnough/gopherjs/xjs"
 	"honnef.co/go/js/dom"
 )
+
+var webglI *webglengine
 
 func main() {
 	dom.GetWindow().AddEventListener("load", false, func(dom.Event) {
 		if err := run(); err != nil {
 			xjs.Alert(err)
 		}
+
 	})
+	webglI = &webgl.Instance
 }
