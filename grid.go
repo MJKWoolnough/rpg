a0de6d0f3d3f5f2ab06838cd1542e5249fca0e62
Michael Woolnough <michael.woolnough@gmail.com>
2017-04-10 17:01:10 +0100

    square grid now draws at correct ratio

--- grid.go
+++ grid.go
@@ -1,6 +1,9 @@
 package main
 
-import "image/color"
+import (
+	"image"
+	"image/color"
+)
 
 type GridType uint8
 
@@ -18,12 +21,18 @@ func (g *Grid) Adjacent(x, y int) [][2]int {
 	return nil
 }
 
-func (g *Grid) Draw(c color.Color) {
+func (g *Grid) Draw(c color.Color, ratio float64, bounds image.Rectangle) {
+	var xScale, yScale float64 = 1, 1
+	if ratio > 1 {
+		xScale = 1 / ratio
+	} else if ratio < 1 {
+		yScale = ratio
+	}
 	switch g.typ {
 	case GridHex:
-		drawHexGrid(c)
+		drawHexGrid(c, xScale, yScale)
 	default:
-		drawSquareGrid(c)
+		drawSquareGrid(c, xScale, yScale)
 	}
 }
 
@@ -31,14 +40,16 @@ func (g *Grid) ScreenCoords(x, y int) (int, int) {
 	return 0, 0
 }
 
-func drawSquareGrid(c color.Color) {
-	for i := float64(-1); i < 1; i += 0.1 {
+func drawSquareGrid(c color.Color, xScale, yScale float64) {
+	for i := float64(-1); i < 1; i += xScale / 10 {
 		drawLine(c, xyz{i, -1, 0}, xyz{i, 1, 0})
+	}
+	for i := float64(-1); i < 1; i += yScale / 10 {
 		drawLine(c, xyz{-1, i, 0}, xyz{1, i, 0})
 	}
 }
 
-func drawHexGrid(c color.Color) {
+func drawHexGrid(c color.Color, xScale, yScale float64) {
 	offset := true
 	for j := float64(-1); j <= 1.07; j += 0.07 {
 		start := float64(-1)
